851221c42d529a358065d033f6b9e1bc6286c0c2
Mirjami Laiho <mirjami.laiho@tuni.fi>
2021-12-30 19:31:52 +0200

    Add index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+const connection = require("./database/database.js");
+const routes = require("./routes/words.js");
+const express = require("express");
+const app = express();
+
+const server = app.listen(8080, async () => {
+  console.log(`Listening on port ${server.address().port}`);
+});
+
+app.use("/words", routes);
+
+const shutdown = () => {
+  console.log("Closing HTTP server");
+  server.close(async () => {
+    console.log("HTTP server closed");
+    try {
+      await connection.close();
+      console.log("mysql connection closed");
+    } catch (err) {
+      console.log(err);
+    }
+    process.exit(0);
+  });
+};
+
+process.on("SIGINT", shutdown);
+process.on("SIGTERM", shutdown);
