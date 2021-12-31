9d3eef758dc587b3143baf8325f74c49c6f26bca
Mirjami Laiho <mirjami.laiho@tuni.fi>
2021-12-31 20:28:22 +0200

    Modify AdminComponent.js: add delete icon

--- frontend/src/components/AdminComponent.js
+++ frontend/src/components/AdminComponent.js
@@ -1,14 +1,20 @@
 import * as React from "react";
 import "../App.css";
 import { AddWords } from "./AddWords";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 export class AdminComponent extends React.Component {
   render() {
     let words = this.props.data.map((words) => (
-      <p>
-        <span key={words.id}>
-          Finnish: {words.fin_word} <b>|</b> English: {words.en_word}
-        </span>
+      <p key={words.id}>
+        Finnish: {words.fin_word} <b>|</b> English: {words.en_word}{" "}
+        <IconButton
+          aria-label="delete"
+          onClick={() => this.props.handleDelete(words.id)}
+        >
+          <DeleteIcon />
+        </IconButton>
       </p>
     ));
 
