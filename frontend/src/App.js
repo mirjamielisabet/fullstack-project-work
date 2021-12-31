bb960174f977c8604b8fe19e8fbf2dd6f737d79f
Mirjami Laiho <mirjami.laiho@tuni.fi>
2021-12-31 17:52:33 +0200

    Modify App.js: add the getData function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,18 +4,55 @@ import { MainComponent } from "./components/MainComponent";
 import { UserComponent } from "./components/UserComponent";
 import { AdminComponent } from "./components/AdminComponent";
 import { NavigationBar } from "./components/NavigationComponent";
+import React from "react";
+const axios = require("axios");
 
-function App() {
-  return (
-    <BrowserRouter>
-      <NavigationBar />
-      <Routes>
-        <Route path="/" element={<MainComponent />} />
-        <Route path="/user" element={<UserComponent />} />
-        <Route path="/teacher" element={<AdminComponent />} />
-      </Routes>
-    </BrowserRouter>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+    };
+    this.getData = this.getData.bind(this);
+  }
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  getData() {
+    axios
+      .get("http://localhost:8080/words")
+      .then((result) => {
+        this.setState({
+          data: result.data.concat(this.state.data),
+        });
+      })
+      .catch((error) => {
+        this.setState({
+          error,
+        });
+      });
+  }
+
+  render() {
+    console.log(this.state.data);
+    return (
+      <BrowserRouter>
+        <NavigationBar />
+        <Routes>
+          <Route path="/" element={<MainComponent />} />
+          <Route path="/user" element={<UserComponent />} />
+          <Route
+            path="/teacher"
+            element={
+              <AdminComponent data={this.state.data} getData={this.getData} />
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
