f80b2c6addf2fbe9b90be2726b8e4950f5fc98d6
Mirjami Laiho <mirjami.laiho@tuni.fi>
2022-01-03 23:26:47 +0200

    Add HeaderComponent.js: contains the header of the App

---
+++ frontend/src/components/HeaderComponent.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../App.css";
+
+export class Header extends React.Component {
+  render() {
+    return (
+      <div className="header">
+        <p>
+          Learn English
+          <span class="material-icons">circle</span>
+        </p>
+      </div>
+    );
+  }
+}
