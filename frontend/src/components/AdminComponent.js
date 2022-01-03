85bbd035a453cf5e84c2c789b4e9dc170c2af2fd
Mirjami Laiho <mirjami.laiho@tuni.fi>
2022-01-03 19:34:31 +0200

    Modify AdminComponent.js: add edit button and the EditWords component for editing saved words

--- frontend/src/components/AdminComponent.js
+++ frontend/src/components/AdminComponent.js
@@ -1,14 +1,22 @@
 import * as React from "react";
 import "../App.css";
 import { AddWords } from "./AddWords";
+import { EditWords } from "./EditWords";
 import IconButton from "@mui/material/IconButton";
 import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
 
 export class AdminComponent extends React.Component {
   render() {
     let words = this.props.data.map((words) => (
       <p key={words.id}>
         Finnish: {words.fin_word} <b>|</b> English: {words.en_word}{" "}
+        <IconButton
+          aria-label="edit"
+          onClick={() => this.props.handleEdit(words)}
+        >
+          <EditIcon />
+        </IconButton>
         <IconButton
           aria-label="delete"
           onClick={() => this.props.handleDelete(words.id)}
@@ -18,16 +26,34 @@ export class AdminComponent extends React.Component {
       </p>
     ));
 
-    return (
-      <div className="container">
-        <p>The Admin view of the Application</p>
-        <br />
-        <h2>Add new words</h2>
-        <AddWords getData={this.props.getData} />
+    if (this.props.editing) {
+      return (
+        <div className="container">
+          <p>The Admin view of the Application</p>
+          <br />
+          <h2>Edit words</h2>
+          <EditWords
+            getData={this.props.getData}
+            setEditingFalse={this.props.setEditingFalse}
+            words={this.props.editingData}
+          />
 
-        <h2>All saved words:</h2>
-        <div>{words}</div>
-      </div>
-    );
+          <h2>All saved words:</h2>
+          <div>{words}</div>
+        </div>
+      );
+    } else {
+      return (
+        <div className="container">
+          <p>The Admin view of the Application</p>
+          <br />
+          <h2>Add new words</h2>
+          <AddWords getData={this.props.getData} />
+
+          <h2>All saved words:</h2>
+          <div>{words}</div>
+        </div>
+      );
+    }
   }
 }
