d31312261ebaa75ecffcfe5983e067ffe8d705d8
Mirjami Laiho <mirjami.laiho@tuni.fi>
2022-01-04 23:13:33 +0200

    Modify MainComponent.js: add the heading for the 'frontpage'

--- frontend/src/components/MainComponent.js
+++ frontend/src/components/MainComponent.js
@@ -5,6 +5,7 @@ export class MainComponent extends React.Component {
   render() {
     return (
       <div className="container">
+        <h1>Home</h1>
         <p>The Front page of the Application</p>
       </div>
     );
