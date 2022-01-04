fabe3c00343d5f5371240c65fe6141a6b4828810
Mirjami Laiho <mirjami.laiho@tuni.fi>
2022-01-04 23:11:46 +0200

    Modify UserComponent.js: add the states for practicing and selecting the language of the word being written: user can choose the word pairs by tag or practice all and choose the given word's language

--- frontend/src/components/UserComponent.js
+++ frontend/src/components/UserComponent.js
@@ -2,8 +2,37 @@ import React from "react";
 import "../App.css";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import { PracticeTable } from "./PracticeTable";
 
 export class UserComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      practicing: false,
+      language: "",
+    };
+    this.resetState = this.resetState.bind(this);
+  }
+
+  setPracticingState() {
+    this.setState({ practicing: true });
+  }
+
+  resetState() {
+    this.setState({ practicing: false, language: "" });
+    this.props.getData();
+  }
+
+  onClick(tag) {
+    this.setState({ practicing: true });
+    this.props.getDataByTag(tag);
+  }
+
+  setLanguage(language) {
+    this.setState({ language: language });
+  }
+
   render() {
     const uniqueTags = [];
     this.props.data.map((words) => {
@@ -13,35 +42,82 @@ export class UserComponent extends React.Component {
       return uniqueTags;
     });
 
-    return (
-      <div className="container">
-        <h1>For Learner</h1>
-        <p>The User view of the Application</p>
-        <br />
-        <h2>Practice</h2>
-        <Stack
-          spacing={1}
-          direction="row"
-          justifyContent="center"
-          alignItems="center"
-          sx={{ margin: "auto" }}
-        >
-          <Button className={"CustomButton"} variant="text">
-            All
+    if (this.state.practicing) {
+      return (
+        <div className="container">
+          <h1>For Learner</h1>
+          <p>The User view of the Application</p>
+          <br />
+          <h2>Practice</h2>
+          <br />
+          <Button
+            className={"CustomButton"}
+            variant="text"
+            onClick={() => this.setLanguage("fin")}
+          >
+            From Finnish to English
+          </Button>{" "}
+          <Button
+            className={"CustomButton"}
+            variant="text"
+            onClick={() => this.setLanguage("en")}
+          >
+            From English to Finnish
           </Button>
-
-          {uniqueTags.map((tag) => (
+          <br />
+          <Button
+            variant="outlined"
+            startIcon={<ArrowBackIcon />}
+            sx={{ ":hover": { color: "#ef4565", border: "1px solid #ef4565" } }}
+            style={{ marginTop: "10px", marginBottom: "20px" }}
+            onClick={() => this.resetState()}
+          >
+            Back
+          </Button>
+          <br />
+          <PracticeTable
+            data={this.props.data}
+            resetState={this.resetState}
+            language={this.state.language}
+          />
+        </div>
+      );
+    } else {
+      return (
+        <div className="container">
+          <h1>For Learner</h1>
+          <p>The User view of the Application</p>
+          <br />
+          <h2>Practice</h2>
+          <Stack
+            spacing={1}
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+            sx={{ margin: "auto" }}
+          >
             <Button
               className={"CustomButton"}
-              key={tag}
               variant="text"
-              onClick={() => this.props.findByTag(tag)}
+              onClick={() => this.setPracticingState()}
             >
-              {tag}
+              All
             </Button>
-          ))}
-        </Stack>
-      </div>
-    );
+
+            {uniqueTags.map((tag) => (
+              <Button
+                className={"CustomButton"}
+                key={tag}
+                variant="text"
+                onClick={() => this.onClick(tag)}
+              >
+                {tag}
+              </Button>
+            ))}
+          </Stack>
+          <br />
+        </div>
+      );
+    }
   }
 }
