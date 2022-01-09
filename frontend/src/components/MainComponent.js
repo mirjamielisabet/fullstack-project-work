ec85c2a768486ce63a36951e5d61dd12f84a52d4
Mirjami Laiho <mirjami.laiho@tuni.fi>
2022-01-09 12:31:09 +0200

    Edit MainComponent.js: add information about the application

--- frontend/src/components/MainComponent.js
+++ frontend/src/components/MainComponent.js
@@ -5,8 +5,21 @@ export class MainComponent extends React.Component {
   render() {
     return (
       <div className="container">
-        <h1>Home</h1>
-        <p>The Front page of the Application</p>
+        <h1>Welcome</h1>
+        <p>
+          Tervetuloa oppimaan englantia! <br /> <i>Welcome to learn English!</i>
+        </p>
+
+        <p>
+          Tämän sovelluksen avulla voit oppia uusia sanoja englanniksi.
+          Harjoittelu sujuu kätevästi <i>"Learner"</i> -sivulla, opettajasi
+          tietokantaan lisäämien sanaparien avulla.
+        </p>
+        <p>
+          Opettajan näkymä löytyy <i>"Teacher"</i> -sivulta. Siellä on
+          mahdollista lisätä uusia sanapareja sekä muokata tai poistaa jo
+          aiemmin lisättyjä.
+        </p>
       </div>
     );
   }
