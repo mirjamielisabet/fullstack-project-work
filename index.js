23077e8196dfef49fc4ae4f2148929a289068514
Mirjami Laiho <mirjami.laiho@tuni.fi>
2022-01-11 13:42:52 +0200

    Edit index.js: add documentation

--- index.js
+++ index.js
@@ -6,17 +6,27 @@ const cors = require("cors");
 const port = process.env.PORT || 8080;
 
 /**
- *
+ * @fileoverview Defines the server, by using middleware functions starts routing and defines the shutdown method.
  * @author Mirjami Laiho
+ */
+
+/**
+ * Starts a server.
  */
 const server = app.listen(port, async () => {
   console.log(`Listening on port ${server.address().port}`);
 });
 
+/**
+ * Middleware functions: enables cors requests, serves the frontend as a static file and directs the requests to the router.
+ */
 app.use(cors());
 app.use(express.static("frontend/build"));
 app.use("/words", routes);
 
+/**
+ * Closes the server and mysql connection.
+ */
 const shutdown = () => {
   console.log("Closing HTTP server");
   server.close(async () => {
